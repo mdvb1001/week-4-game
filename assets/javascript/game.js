06ba5a0920fa5f642a4f5731ab823bed3be11f51
maxvanbel <Max@Maximes-MacBook-Pro.local>
2016-10-26 11:55:26 -0500

    fix the deleted images folder

---
+++ assets/javascript/game.js
@@ -0,0 +1,29 @@
+
+
+
+// PHASE ONE: 
+
+// Create the layout of the html page:
+// Header: "CrystalsCollector" around a 1/3 of the page 
+// Jumbotron with instruction inside, around 1/2 of the page 
+// A div #numGl of about 1/4, and another smaller div around a 1/12
+// 4 Buttons for the crystals themselves 
+// a skin div, 1/4 of the page 
+// An identitical div to #numGoal in size for #numUser 
+// All these divs should add up to the height of the html page
+
+// Bootstrap theme: something simple, easy
+// colors can be done with CSS 
+// Background: find something funky and funny 
+// Find images for the crystals
+
+
+
+
+// PHASE TWO: 
+// 
+//
+// 
+// 
+// 
+// 
