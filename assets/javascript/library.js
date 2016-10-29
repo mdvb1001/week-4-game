5356f52045c0b08dcf7c5b170793404943a3bb97
maxvanbel <Max@Maximes-MacBook-Pro.local>
2016-10-29 09:37:32 -0500

    reorganize scope with Joe

--- assets/javascript/library.js
+++ assets/javascript/library.js
@@ -57,169 +57,10 @@ $(document).ready(function() {
         $('#goalNum').html('Your goal:' + ' ' + random);
 
 
-        $('#crystal-1').on('click', function() {
-            // blueCrytal = randomCrystalNumber[0];
-            // console.log(blueCrytal);
-            // var button = guess[0];
-            // console.log(button);
-            $('#expression').html('');
-            result += guess[0];
-            console.log(result);
-            $('#score').html('Your score:' + ' ' + result);
-
-            if (random === result) {
-                wins++;
-                $('#expression').html('You Win!');
-                $('#userWins').html('Wins:' + ' ' + wins);
-                reset();
-
-                // random = 0;
-                // $('#goal').html('Your goal:' + ' ' + random);
-                // result = 0;
-                // $('#score').html('Your score:' + ' ' + result);
 
 
 
-            } else if (result > random) {
-                loses++;
-                $('#expression').html('You lose!');
-                $('#userLoses').html('Loses:' + ' ' + loses);
-                reset();
 
-                // random = 0;
-                // $('#goal').html('Your goal:' + ' ' + random);
-                // result = 0;
-                // $('#score').html('Your score:' + ' ' + result);
-
-
-
-            }
-
-        });
-
-        $('#crystal-2').on('click', function() {
-            // blueCrytal = randomCrystalNumber[0];
-            // console.log(blueCrytal);
-            // var button = guess[1];
-            // console.log(button);
-            $('#expression').html('');
-            result += guess[1];
-            console.log(result);
-            $('#score').html('Your score:' + ' ' + result);
-
-            if (random === result) {
-                wins++;
-                $('#expression').html('You Win!');
-                $('#userWins').html('Wins:' + ' ' + wins);
-                reset();
-                // random = 0;
-                // $('#goal').html('Your goal:' + ' ' + random);
-                // result === 0;
-                // $('#score').html('Your score:' + ' ' + result);
-                // playGame();
-
-
-            } else if (result > random) {
-                loses++;
-                $('#expression').html('You lose!');
-                $('#userLoses').html('Loses:' + ' ' + loses);
-                reset();
-
-                // random = 0;
-                // $('#goal').html('Your goal:' + ' ' + random);
-                // result === 0;
-                // $('#score').html('Your score:' + ' ' + result);
-                // playGame();
-            }
-
-        });
-
-        $('#crystal-3').on('click', function() {
-            // blueCrytal = randomCrystalNumber[0];
-            // console.log(blueCrytal);
-            // var button = guess[2];
-            // console.log(button);
-            $('#expression').html('');
-            result += guess[2];
-            console.log(result);
-            $('#score').html('Your score:' + ' ' + result);
-
-            if (random === result) {
-                wins++;
-                $('#expression').html('You Win!');
-                $('#userWins').html('Wins:' + ' ' + wins);
-                reset();
-
-                // random = 0;
-                // $('#goal').html('Your goal:' + ' ' + random);
-                // result = 0;
-                // $('#score').html('Your score:' + ' ' + result);
-                // playGame();
-
-
-            } else if (result > random) {
-                loses++;
-                $('#expression').html('You lose!');
-                $('#userLoses').html('Loses:' + ' ' + loses);
-                reset();
-
-                // guess = new Array(0);
-                // random = 0;
-                // $('#goal').html('Your goal:' + ' ' + random);
-                // result = 0;
-                // $('#score').html('Your score:' + ' ' + result);
-                // playGame();
-            }
-
-        });
-
-        $('#crystal-4').on('click', function() {
-            // blueCrytal = randomCrystalNumber[0];
-            // console.log(blueCrytal);
-            // var button = guess[3];
-            // console.log(button);
-            $('#expression').html('');
-            result += guess[3];
-            console.log(result);
-            $('#score').html('Your score:' + ' ' + result);
-
-            if (random === result) {
-                wins++;
-                $('#expression').html('You Win!');
-                $('#userWins').html('Wins:' + ' ' + wins);
-                reset();
-
-                // random = 0;
-                // $('#goal').html('Your goal:' + ' ' + random);
-                // result = 0;
-                // $('#score').html('Your score:' + ' ' + result);
-                // playGame();
-
-
-            } else if (result > random) {
-                loses++;
-                $('#expression').html('You lose!');
-                $('#userLoses').html('Loses:' + ' ' + loses);
-                reset();
-
-                // guess = new Array(0);
-                // random = 0;
-                // $('#goal').html('Your goal:' + ' ' + random);
-                // result = 0;
-                // $('#score').html('Your score:' + ' ' + result);
-                // playGame();
-            }
-
-        });
-
-        function reset() {
-            random = '';
-            $('#goalNum').html('Your goal:' + ' ' + random);
-            result = 0;
-            $('#score').html('Your score:' + ' ' + result);
-            playGame();
-
-        }
 
 
 
@@ -236,6 +77,170 @@ $(document).ready(function() {
 
     };
     playGame();
+
+    function reset() {
+        random = '';
+        $('#goalNum').html('Your goal:' + ' ' + random);
+        result = 0;
+        $('#score').html('Your score:' + ' ' + result);
+        playGame();
+
+    }
+
+    $('#crystal-1').on('click', function() {
+        // blueCrytal = randomCrystalNumber[0];
+        // console.log(blueCrytal);
+        // var button = guess[0];
+        // console.log(button);
+        $('#expression').html('');
+        result += guess[0];
+        console.log(result);
+        $('#score').html('Your score:' + ' ' + result);
+
+        if (random === result) {
+            wins++;
+            $('#expression').html('You Win!');
+            $('#userWins').html('Wins:' + ' ' + wins);
+            reset();
+
+            // random = 0;
+            // $('#goal').html('Your goal:' + ' ' + random);
+            // result = 0;
+            // $('#score').html('Your score:' + ' ' + result);
+
+
+
+        } else if (result > random) {
+            loses++;
+            $('#expression').html('You lose!');
+            $('#userLoses').html('Loses:' + ' ' + loses);
+            reset();
+
+            // random = 0;
+            // $('#goal').html('Your goal:' + ' ' + random);
+            // result = 0;
+            // $('#score').html('Your score:' + ' ' + result);
+
+
+
+        }
+
+    });
+
+    $('#crystal-2').on('click', function() {
+        // blueCrytal = randomCrystalNumber[0];
+        // console.log(blueCrytal);
+        // var button = guess[1];
+        // console.log(button);
+        $('#expression').html('');
+        result += guess[1];
+        console.log(result);
+        $('#score').html('Your score:' + ' ' + result);
+
+        if (random === result) {
+            wins++;
+            $('#expression').html('You Win!');
+            $('#userWins').html('Wins:' + ' ' + wins);
+            reset();
+            // random = 0;
+            // $('#goal').html('Your goal:' + ' ' + random);
+            // result === 0;
+            // $('#score').html('Your score:' + ' ' + result);
+            // playGame();
+
+
+        } else if (result > random) {
+            loses++;
+            $('#expression').html('You lose!');
+            $('#userLoses').html('Loses:' + ' ' + loses);
+            reset();
+
+            // random = 0;
+            // $('#goal').html('Your goal:' + ' ' + random);
+            // result === 0;
+            // $('#score').html('Your score:' + ' ' + result);
+            // playGame();
+        }
+
+    });
+
+    $('#crystal-3').on('click', function() {
+        // blueCrytal = randomCrystalNumber[0];
+        // console.log(blueCrytal);
+        // var button = guess[2];
+        // console.log(button);
+        $('#expression').html('');
+        result += guess[2];
+        console.log(result);
+        $('#score').html('Your score:' + ' ' + result);
+
+        if (random === result) {
+            wins++;
+            $('#expression').html('You Win!');
+            $('#userWins').html('Wins:' + ' ' + wins);
+            reset();
+
+            // random = 0;
+            // $('#goal').html('Your goal:' + ' ' + random);
+            // result = 0;
+            // $('#score').html('Your score:' + ' ' + result);
+            // playGame();
+
+
+        } else if (result > random) {
+            loses++;
+            $('#expression').html('You lose!');
+            $('#userLoses').html('Loses:' + ' ' + loses);
+            reset();
+
+            // guess = new Array(0);
+            // random = 0;
+            // $('#goal').html('Your goal:' + ' ' + random);
+            // result = 0;
+            // $('#score').html('Your score:' + ' ' + result);
+            // playGame();
+        }
+
+    });
+
+    $('#crystal-4').on('click', function() {
+        // blueCrytal = randomCrystalNumber[0];
+        // console.log(blueCrytal);
+        // var button = guess[3];
+        // console.log(button);
+        $('#expression').html('');
+        result += guess[3];
+        console.log(result);
+        $('#score').html('Your score:' + ' ' + result);
+
+        if (random === result) {
+            wins++;
+            $('#expression').html('You Win!');
+            $('#userWins').html('Wins:' + ' ' + wins);
+            reset();
+
+            // random = 0;
+            // $('#goal').html('Your goal:' + ' ' + random);
+            // result = 0;
+            // $('#score').html('Your score:' + ' ' + result);
+            // playGame();
+
+
+        } else if (result > random) {
+            loses++;
+            $('#expression').html('You lose!');
+            $('#userLoses').html('Loses:' + ' ' + loses);
+            reset();
+
+            // guess = new Array(0);
+            // random = 0;
+            // $('#goal').html('Your goal:' + ' ' + random);
+            // result = 0;
+            // $('#score').html('Your score:' + ' ' + result);
+            // playGame();
+        }
+
+    });
 
     // function checkWinsOrLoses() {
 
